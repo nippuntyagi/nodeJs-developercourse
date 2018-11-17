38d792f7db8b18a005fe0b8c7906d8d9ea78713e
Nippun Tyagi <nippuntyagi@gmail.com>
2018-11-17 14:19:57 +0530

    Express Middleware
    # writing logs to file
    # maintenance page route

--- node-web-servers/server.js
+++ node-web-servers/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -9,8 +10,23 @@ hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 
 // Add middleware
-app.use(express.static(__dirname+'/public'));
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append to the server.log.');
+        }
+    });
+    next();
+});
 
+// app.use((req, res, next)=>{
+//    res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
 // partials helpers
 hbs.registerHelper('getCurrentYear', ()=>{
     return new Date().getFullYear();
