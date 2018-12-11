110c0cf86e9a87d950d9b9ffdf6fe0ff7154372d
Nippun Tyagi <nippuntyagi@gmail.com>
2018-12-11 22:14:01 +0530

    setting up user model

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,12 +1,35 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('Users',{
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique:1,
+        validate: {
+            validator: (value)=>{
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access:{
+            type: String,
+            required: true,
+        },
+        token:{
+            type: String,
+            required: true,
+        }
+    }]
 });
 
 module.exports = {User};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -98,6 +98,16 @@ app.patch('/todos/:id',(req, res)=>{
     })
 });
 
+app.post('/users', (req, res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then((user)=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+})
 var port = process.env.PORT;
 
 app.listen(port, ()=>{
